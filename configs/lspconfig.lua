9fc6db592ddb21e8964ae797df8605b8ebce3918
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2024-07-02 20:53:11 +0200

    feat: add ccls support

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,10 +1,9 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
-
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "denols", "tsserver", "clangd", "pyright" }
+local servers = { "html", "cssls", "denols", "tsserver", "ccls", "clangd", "pyright" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -12,9 +11,6 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
-
---
--- lspconfig.pyright.setup { blabla}
 
 lspconfig.denols.setup {
   on_attach = on_attach,
@@ -25,4 +21,20 @@ lspconfig.tsserver.setup {
   on_attach = on_attach,
   root_dir = lspconfig.util.root_pattern "package.json",
   single_file_support = false,
+}
+
+lspconfig.ccls.setup {
+  init_options = {
+    cache = {
+      directory = ".ccls-cache",
+    },
+  },
+}
+
+lspconfig.ccls.setup {
+  cmd = {
+    "clangd",
+    "--background-index",
+    -- "--query-driver=/Users/sullrich/.platformio/packages/toolchain-xtensa-esp32s3/bin/xtensa-esp32s3-elf-gcc*"
+  },
 }
